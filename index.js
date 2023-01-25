ede6d9f3d86ee16e0fcc8bcc48721c7ded969ea1
Laura Lopez Alonso <laura.lopez.alo@gmail.com>
2023-01-25 20:03:16 +0100

    Nivell 3

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var button = document.querySelector('button');
 var joke = document.querySelector('p');
 var scoreJokes = [];
+/**
+ * "click" event on "button" that executes two functions:
+ * "changeBackgroundColor()" and
+ * "giveMeAJoke(genre)", to which it passes a random parameter of 1 or 2;
+ */
 var score = 1;
 button.addEventListener('click', function () {
+    changeBackgroundColor();
     var genre = Math.floor(Math.random() * (3 - 1) + 1);
     giveMeAJoke(genre);
 });
+/**
+ * Function that uses a fetch request to get a joke from one of two different APIs depending on the value of "genre", displays the joke in an HTML element, and stores it in an array along with its rating and date. It also calls the function "starsRating()".
+ * @param genre If "genre" equals 1, it gets a joke from "icanhazdadjoke.com", and if it's different from 1, it gets a joke from "api.chucknorris.io".
+ */
 function giveMeAJoke(genre) {
     stars.style.display = "flex";
     var url1 = 'https://icanhazdadjoke.com/';
@@ -24,40 +34,86 @@ function giveMeAJoke(genre) {
         .then(function (response) { return response.json(); })
         .then(function (data) { genre == 1 ? joke.innerHTML = "".concat(data.joke, "<br><span>-Dad</span>") : joke.innerHTML = "".concat(data.value, "<br><span>-Chuck Norris</span>"); });
     starsRating();
+    console.log(scoreJokes);
 }
 var stars = document.getElementById('stars');
 var star1 = document.getElementById('star1');
 var star2 = document.getElementById('star2');
 var star3 = document.getElementById('star3');
+/**
+ * Function that adds "click" events to the rating "stars" to obtain the punctuation of each joke, at the same time that it modifies the format of these depending on whether they have been marked or unmarked.
+ */
 function starsRating() {
     star1.addEventListener('click', function () {
         score = 1;
-        star2.style.color = "rgb(165, 164, 164)";
-        star3.style.color = "rgb(165, 164, 164)";
+        star2.style.color = "rgba( 255, 255, 255, 0.35 )";
+        star3.style.color = "rgba( 255, 255, 255, 0.35 )";
     });
     star2.addEventListener('click', function () {
         score = 2;
         star2.style.color = "gold";
-        star3.style.color = "rgb(165, 164, 164)";
+        star3.style.color = "rgba( 255, 255, 255, 0.35 )";
     });
     star3.addEventListener('click', function () {
         score = 3;
         star2.style.color = "gold";
         star3.style.color = "gold";
     });
-    star2.style.color = "rgb(165, 164, 164)";
-    star3.style.color = "rgb(165, 164, 164)";
-    console.log(scoreJokes);
+    star2.style.color = "rgba( 255, 255, 255, 0.35 )";
+    star3.style.color = "rgba( 255, 255, 255, 0.35 )";
 }
 var weatherToday = document.getElementById('weatherToday');
 var weatherTomorrow = document.getElementById('weatherTomorrow');
+var weatherDayAfterTomorrow = document.getElementById('weatherDayAfterTomorrow');
+/**
+ * "load" event of the window object that executes a fetch request to a weather API and displays the data on the web
+ */
 window.addEventListener('load', function () {
-    fetch('https://api.open-meteo.com/v1/forecast?latitude=41.39&longitude=2.16&models=best_match&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=Europe%2FLondon&hourly=rain', {
+    fetch('https://api.open-meteo.com/v1/forecast?latitude=41.39&longitude=2.16&models=best_match&hourly=weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=Europe%2FLondon&hourly=rain', {
         headers: { Accept: 'application/json' }
     })
         .then(function (response) { return response.json(); })
         .then(function (data) {
-        weatherToday.innerHTML = "<h3>Today</h3><br><h4>".concat(data.daily.time[0], "</h4><br>\n            Temperature: ").concat(data.current_weather.temperature, " ").concat(data.daily_units.temperature_2m_max, " <br>\n            Sunrise: ").concat(data.daily.sunrise[0].substring(data.daily.sunset[0].length - 5), " <br>\n            Sunset: ").concat(data.daily.sunset[0].substring(data.daily.sunset[0].length - 5));
-        weatherTomorrow.innerHTML = "<h3>Tomorrox</h3><br><h4>".concat(data.daily.time[1], "</h4><br>\n            Temperature: ").concat(data.current_weather.temperature, " ").concat(data.daily_units.temperature_2m_max, " <br>");
+        weatherToday.innerHTML = "<div class=\"day-title\">Today, ".concat(whatDayIsIt(data.daily.time[0]), "</div>\n            <div id=\"today-icon-and-temperature\"> \n                <img src=\"./src/weather-icon-").concat(data.current_weather.weathercode == 0 ? "sun"
+            : data.current_weather.weathercode < 3 ? "cloud-sun"
+                : data.current_weather.weathercode < 50 ? "cloud"
+                    : data.current_weather.weathercode < 70 ? "rain"
+                        : data.current_weather.weathercode < 80 ? "snow" : "storm", ".svg\" alt=\"\">\n                ").concat(data.current_weather.temperature, " \u00BAC\n            </div>\n            <div class=\"max-min\">\n                <div>\n                    <img src=\"./src/weather-icon-max.svg\" alt=\"\"> \n                    <div>max. <br> ").concat(data.daily.temperature_2m_max[0], "\u00BAC </div> \n                </div>\n                <div>\n                    <img src=\"./src/weather-icon-min.svg\" alt=\"\">\n                    <div>min. <br> ").concat(data.daily.temperature_2m_min[0], "\u00BAC</div>\n                </div>\n            </div>");
+        weatherTomorrow.innerHTML = "<div class=\"day-title\">".concat(whatDayIsIt(data.daily.time[1]), "</div>\n            <div class=\"next-days-icon-and-temperature\"> \n                <img src=\"./src/weather-icon-").concat(data.daily.weathercode[1] == 0 ? "sun"
+            : data.daily.weathercode[1] < 3 ? "cloud-sun"
+                : data.daily.weathercode[1] < 50 ? "cloud"
+                    : data.daily.weathercode[1] < 70 ? "rain"
+                        : data.daily.weathercode[1] < 80 ? "snow" : "storm", ".svg\" alt=\"\">\n                <div class=\"max-min max-min-next\">\n                    <div>\n                        <img src=\"./src/weather-icon-max.svg\" alt=\"\"> \n                        <div>max. <br> ").concat(data.daily.temperature_2m_max[1], "\u00BAC </div> \n                    </div>\n                    <div>\n                        <img src=\"./src/weather-icon-min.svg\" alt=\"\">\n                        <div>min. <br> ").concat(data.daily.temperature_2m_min[1], "\u00BAC</div>\n                    </div> \n                </div>\n            </div>");
+        weatherDayAfterTomorrow.innerHTML = "<div class=\"day-title\">".concat(whatDayIsIt(data.daily.time[2]), "</div>\n            <div class=\"next-days-icon-and-temperature\"> \n                <img src=\"./src/weather-icon-").concat(data.daily.weathercode[2] == 0 ? "sun"
+            : data.daily.weathercode[2] < 3 ? "cloud-sun"
+                : data.daily.weathercode[2] < 50 ? "cloud"
+                    : data.daily.weathercode[2] < 70 ? "rain"
+                        : data.daily.weathercode[2] < 80 ? "snow" : "storm", ".svg\" alt=\"\">\n                <div class=\"max-min max-min-next\">\n                    <div>\n                        <img src=\"./src/weather-icon-max.svg\" alt=\"\"> \n                        <div>max. <br> ").concat(data.daily.temperature_2m_max[2], "\u00BAC </div> \n                    </div>\n                    <div>\n                        <img src=\"./src/weather-icon-min.svg\" alt=\"\">\n                        <div>min. <br> ").concat(data.daily.temperature_2m_min[2], "\u00BAC</div>\n                    </div> \n                </div>\n            </div>");
     });
 });
+/**
+ * Function that modifies the format of a date
+ * @param day date to format
+ * @returns formatted date ("January 17")
+ */
+function whatDayIsIt(day) {
+    var date = new Date(day);
+    var formatter = new Intl.DateTimeFormat("en-US", { month: "long", day: "numeric" });
+    var formattedDate = formatter.format(date);
+    return formattedDate;
+}
+var weatherNextDays = document.getElementById('weatherNextDays');
+function show() {
+    weatherNextDays.style.display = 'block';
+}
+function hide() {
+    weatherNextDays.style.display = 'none';
+}
+var mainContainer = document.getElementById('container');
+var i = 2;
+function changeBackgroundColor() {
+    if (i == 6)
+        i = 1;
+    mainContainer.style.backgroundImage = "url('./src/blob".concat(i, ".svg')");
+    i++;
+}
--- index.ts
+++ index.ts
@@ -1,14 +1,26 @@
 const button: HTMLButtonElement = <HTMLButtonElement>document.querySelector('button');
 const joke: HTMLParagraphElement = <HTMLParagraphElement>document.querySelector('p');
-
-
 const scoreJokes: { joke: string; score: number; date: string }[] = [];
+
+/**
+ * "click" event on "button" that executes two functions: 
+ * "changeBackgroundColor()" and
+ * "giveMeAJoke(genre)", to which it passes a random parameter of 1 or 2;
+ */
 
 let score: number = 1;
 button.addEventListener('click', () => {
+    changeBackgroundColor();
     let genre: number = Math.floor(Math.random() * (3 - 1) + 1);
     giveMeAJoke(genre);
 });
+
+
+
+/**
+ * Function that uses a fetch request to get a joke from one of two different APIs depending on the value of "genre", displays the joke in an HTML element, and stores it in an array along with its rating and date. It also calls the function "starsRating()".
+ * @param genre If "genre" equals 1, it gets a joke from "icanhazdadjoke.com", and if it's different from 1, it gets a joke from "api.chucknorris.io".
+ */
 
 function giveMeAJoke(genre: number) {
     stars.style.display = "flex"
@@ -26,9 +38,11 @@ function giveMeAJoke(genre: number) {
         headers: { Accept: 'application/json' },
     })
         .then((response) => response.json())
-        .then((data) => { genre == 1 ? joke.innerHTML = `${data.joke}<br><span>-Dad</span>` : joke.innerHTML = `${data.value}<br><span>-Chuck Norris</span>`});
+        .then((data) => { genre == 1 ? joke.innerHTML = `${data.joke}<br><span>-Dad</span>` : joke.innerHTML = `${data.value}<br><span>-Chuck Norris</span>` });
     starsRating();
+    console.log(scoreJokes);
 }
+
 
 
 const stars: HTMLElement = <HTMLElement>document.getElementById('stars');
@@ -36,47 +50,142 @@ const star1: HTMLElement = <HTMLElement>document.getElementById('star1');
 const star2: HTMLElement = <HTMLElement>document.getElementById('star2');
 const star3: HTMLElement = <HTMLElement>document.getElementById('star3');
 
+/**
+ * Function that adds "click" events to the rating "stars" to obtain the punctuation of each joke, at the same time that it modifies the format of these depending on whether they have been marked or unmarked.
+ */
+
 function starsRating(): void {
     star1.addEventListener('click', () => {
         score = 1;
-        star2.style.color = "rgb(165, 164, 164)"
-        star3.style.color = "rgb(165, 164, 164)"
+        star2.style.color = "rgba( 255, 255, 255, 0.35 )"
+        star3.style.color = "rgba( 255, 255, 255, 0.35 )"
     });
     star2.addEventListener('click', () => {
         score = 2;
         star2.style.color = "gold"
-        star3.style.color = "rgb(165, 164, 164)"
+        star3.style.color = "rgba( 255, 255, 255, 0.35 )"
     });
     star3.addEventListener('click', () => {
         score = 3;
         star2.style.color = "gold"
         star3.style.color = "gold"
     });
-    star2.style.color = "rgb(165, 164, 164)"
-    star3.style.color = "rgb(165, 164, 164)"
-    console.log(scoreJokes);
-
+    star2.style.color = "rgba( 255, 255, 255, 0.35 )"
+    star3.style.color = "rgba( 255, 255, 255, 0.35 )"
 }
+
 
 
 const weatherToday: HTMLDivElement = <HTMLDivElement>document.getElementById('weatherToday');
 const weatherTomorrow: HTMLDivElement = <HTMLDivElement>document.getElementById('weatherTomorrow');
+const weatherDayAfterTomorrow: HTMLDivElement = <HTMLDivElement>document.getElementById('weatherDayAfterTomorrow');
+
+/**
+ * "load" event of the window object that executes a fetch request to a weather API and displays the data on the web
+ */
 
 window.addEventListener('load', () => {
-    fetch('https://api.open-meteo.com/v1/forecast?latitude=41.39&longitude=2.16&models=best_match&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=Europe%2FLondon&hourly=rain', {
+    fetch('https://api.open-meteo.com/v1/forecast?latitude=41.39&longitude=2.16&models=best_match&hourly=weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=Europe%2FLondon&hourly=rain', {
         headers: { Accept: 'application/json' },
     })
         .then((response) => response.json())
         .then((data) => {
-            weatherToday.innerHTML = `<h3>Today</h3><br><h4>${data.daily.time[0]}</h4><br>
-            Temperature: ${data.current_weather.temperature} ${data.daily_units.temperature_2m_max} <br>
-            Sunrise: ${data.daily.sunrise[0].substring(data.daily.sunset[0].length - 5)} <br>
-            Sunset: ${data.daily.sunset[0].substring(data.daily.sunset[0].length - 5)}`;
-            weatherTomorrow.innerHTML = `<h3>Tomorrox</h3><br><h4>${data.daily.time[1]}</h4><br>
-            Temperature: ${data.current_weather.temperature} ${data.daily_units.temperature_2m_max} <br>`
+            weatherToday.innerHTML = `<div class="day-title">Today, ${whatDayIsIt(data.daily.time[0])}</div>
+            <div id="today-icon-and-temperature"> 
+                <img src="./src/weather-icon-${data.current_weather.weathercode == 0 ? "sun"
+                        : data.current_weather.weathercode < 3 ? "cloud-sun"
+                            : data.current_weather.weathercode < 50 ? "cloud"
+                                : data.current_weather.weathercode < 70 ? "rain"
+                                    : data.current_weather.weathercode < 80 ? "snow" : "storm"}.svg" alt="">
+                ${data.current_weather.temperature} ºC
+            </div>
+            <div class="max-min">
+                <div>
+                    <img src="./src/weather-icon-max.svg" alt=""> 
+                    <div>max. <br> ${data.daily.temperature_2m_max[0]}ºC </div> 
+                </div>
+                <div>
+                    <img src="./src/weather-icon-min.svg" alt="">
+                    <div>min. <br> ${data.daily.temperature_2m_min[0]}ºC</div>
+                </div>
+            </div>`;
+
+            weatherTomorrow.innerHTML = `<div class="day-title">${whatDayIsIt(data.daily.time[1])}</div>
+            <div class="next-days-icon-and-temperature"> 
+                <img src="./src/weather-icon-${data.daily.weathercode[1] == 0 ? "sun"
+                        : data.daily.weathercode[1] < 3 ? "cloud-sun"
+                            : data.daily.weathercode[1] < 50 ? "cloud"
+                                : data.daily.weathercode[1] < 70 ? "rain"
+                                    : data.daily.weathercode[1] < 80 ? "snow" : "storm"}.svg" alt="">
+                <div class="max-min max-min-next">
+                    <div>
+                        <img src="./src/weather-icon-max.svg" alt=""> 
+                        <div>max. <br> ${data.daily.temperature_2m_max[1]}ºC </div> 
+                    </div>
+                    <div>
+                        <img src="./src/weather-icon-min.svg" alt="">
+                        <div>min. <br> ${data.daily.temperature_2m_min[1]}ºC</div>
+                    </div> 
+                </div>
+            </div>`;
+            
+            weatherDayAfterTomorrow.innerHTML = `<div class="day-title">${whatDayIsIt(data.daily.time[2])}</div>
+            <div class="next-days-icon-and-temperature"> 
+                <img src="./src/weather-icon-${data.daily.weathercode[2] == 0 ? "sun"
+                        : data.daily.weathercode[2] < 3 ? "cloud-sun"
+                            : data.daily.weathercode[2] < 50 ? "cloud"
+                                : data.daily.weathercode[2] < 70 ? "rain"
+                                    : data.daily.weathercode[2] < 80 ? "snow" : "storm"}.svg" alt="">
+                <div class="max-min max-min-next">
+                    <div>
+                        <img src="./src/weather-icon-max.svg" alt=""> 
+                        <div>max. <br> ${data.daily.temperature_2m_max[2]}ºC </div> 
+                    </div>
+                    <div>
+                        <img src="./src/weather-icon-min.svg" alt="">
+                        <div>min. <br> ${data.daily.temperature_2m_min[2]}ºC</div>
+                    </div> 
+                </div>
+            </div>`
         });
 });
 
+
+
+/**
+ * Function that modifies the format of a date
+ * @param day date to format
+ * @returns formatted date ("January 17")
+ */
+
+function whatDayIsIt(day: string) {
+    let date = new Date(day);
+    let formatter = new Intl.DateTimeFormat("en-US", { month: "long", day: "numeric" });
+    let formattedDate = formatter.format(date);
+    return formattedDate;
+}
+
+
+const weatherNextDays: HTMLDivElement = <HTMLDivElement>document.getElementById('weatherNextDays');
+
+
+function show() {
+    weatherNextDays.style.display = 'block'
+}
+
+function hide() {
+    weatherNextDays.style.display = 'none'
+}
+
+
+const mainContainer: HTMLDivElement = <HTMLDivElement>document.getElementById('container');
+let i: number = 2;
+
+function changeBackgroundColor() {
+    if (i == 6) i = 1;
+    mainContainer.style.backgroundImage = `url('./src/blob${i}.svg')`;
+    i++;
+}
 
 
 
